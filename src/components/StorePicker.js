9d02d49d5fc6f42fb0761ffab3488bcede7c478a
Alex Cyrus <alex@alexcyrus.com>
2018-06-03 17:53:16 -0400

    creating ref

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-	goToStore(event) {
+	myInput = React.createRef();
+
+	goToStore = (event) => {
 		// stops the form from submitting
 		event.preventDefault();
 		// gets the text from that input
+		console.log(this);
 		// changes the page to /store/whatever-they-entered
 	}
 	render() {
 		return (
 			<form className="store-selector" onSubmit={this.goToStore}>
 				<h2>Please Enter A Store</h2>
-				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+				<input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()} />
 				<button type="submit">Visit Store -></button>
 			</form>
 		)
