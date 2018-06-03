734303576905a6343cab51ae8a270c79870bda0c
Alex Cyrus <alex@alexcyrus.com>
2018-06-03 17:21:38 -0400

    adding gotostore event

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+	goToStore(event) {
+		// stops the form from submitting
+		event.preventDefault();
+		// gets the text from that input
+		// changes the page to /store/whatever-they-entered
+	}
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore}>
 				<h2>Please Enter A Store</h2>
 				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
 				<button type="submit">Visit Store -></button>
